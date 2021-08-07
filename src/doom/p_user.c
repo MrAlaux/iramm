8b69628e8e094e725cd1356408cf9fe5d6e3f2f3
Alaux <73968015+MrAlaux@users.noreply.github.com>
2021-08-07 08:57:15 -0300

    Add crouching and VIEWHEIGHT setting
    P_CalcHeight now replaces VIEWHEIGHT with a variable, which gets a value assigned based on the chosen VIEWHEIGHT setting (original or adjusted) and on the player crouching or standing, and uses that variable for view bobbing calculations. Additionally, if the player just stood up after crouching, the "smooth step up" code is executed, resulting in a smooth crouch-to-stand transition.
    
    P_MovePlayer's P_Thrust calls replace the fixed value of 2048 with a variable, which starts at 2048 and gets halved if the player is crouching, and uses that variable for movement calculations.

--- src/doom/p_user.c
+++ src/doom/p_user.c
@@ -41,7 +41,7 @@
 //
 
 // 16 pixels of bob
-#define MAXBOB	0x100000	
+#define MAXBOB	0x100000
 
 // [crispy] variable player view bob
 static const fixed_t crispy_bobfactor[3] = {4, 3, 0};
@@ -57,11 +57,11 @@ void
 P_Thrust
 ( player_t*	player,
   angle_t	angle,
-  fixed_t	move ) 
+  fixed_t	move )
 {
     angle >>= ANGLETOFINESHIFT;
-    
-    player->mo->momx += FixedMul(move,finecosine[angle]); 
+
+    player->mo->momx += FixedMul(move,finecosine[angle]);
     player->mo->momy += FixedMul(move,finesine[angle]);
 }
 
@@ -72,11 +72,12 @@ P_Thrust
 // P_CalcHeight
 // Calculate the walking / running height adjustment
 //
-void P_CalcHeight (player_t* player) 
+void P_CalcHeight (player_t* player)
 {
     int		angle;
     fixed_t	bob;
-    
+    fixed_t view;
+
     // Regular movement bobbing
     // (needs to be calculated for gun swing
     // even if not on ground)
@@ -86,7 +87,7 @@ void P_CalcHeight (player_t* player)
     player->bob =
 	FixedMul (player->mo->momx, player->mo->momx)
 	+ FixedMul (player->mo->momy,player->mo->momy);
-    
+
     player->bob >>= 2;
 
     if (player->bob>MAXBOB)
@@ -95,41 +96,57 @@ void P_CalcHeight (player_t* player)
     // [crispy] variable player view bob
     player->bob2 = crispy_bobfactor[crispy->bobfactor] * player->bob / 4;
 
+    if (crispy->viewheight) { // [IRamm] Check for viewheight setting
+        if (player->mo->flags & MF_CROUCH) {view = ALTCVIEWHEIGHT;}
+        else {view = ALTVIEWHEIGHT;}
+    }
+    else {
+        if (player->mo->flags & MF_CROUCH) {view = CVIEWHEIGHT;}
+        else {view = VIEWHEIGHT;}
+    }
+
+
     if ((player->cheats & CF_NOMOMENTUM) || !onground)
     {
-	player->viewz = player->mo->z + VIEWHEIGHT;
+        player->viewz = player->mo->z + view;
 
-	if (player->viewz > player->mo->ceilingz-4*FRACUNIT)
-	    player->viewz = player->mo->ceilingz-4*FRACUNIT;
+        if (player->viewz > player->mo->ceilingz-4*FRACUNIT)
+            player->viewz = player->mo->ceilingz-4*FRACUNIT;
 
-	// [crispy] fix player viewheight in NOMOMENTUM mode
-	//player->viewz = player->mo->z + player->viewheight;
-	return;
+        // [crispy] fix player viewheight in NOMOMENTUM mode
+        //player->viewz = player->mo->z + player->viewheight;
+        return;
     }
-		
+
     angle = (FINEANGLES/20*leveltime)&FINEMASK;
     bob = FixedMul ( player->bob2/2, finesine[angle]); // [crispy] variable player view bob
 
-    
+
     // move viewheight
     if (player->playerstate == PST_LIVE)
     {
 	player->viewheight += player->deltaviewheight;
 
-	if (player->viewheight > VIEWHEIGHT)
+	if (player->viewheight > view)
 	{
-	    player->viewheight = VIEWHEIGHT;
+	    player->viewheight = view;
 	    player->deltaviewheight = 0;
 	}
 
-	if (player->viewheight < VIEWHEIGHT/2)
+	if (player->viewheight < view/2)
 	{
-	    player->viewheight = VIEWHEIGHT/2;
+	    player->viewheight = view/2;
 	    if (player->deltaviewheight <= 0)
 		player->deltaviewheight = 1;
 	}
-	
-	if (player->deltaviewheight)	
+
+	// [IRamm] Check if player just stood up
+	if (!(player->mo->flags & MF_CROUCH) && player->crouchTics) {
+        player->viewheight -= player->mo->floorz - player->mo->z;
+        player->deltaviewheight = (view - player->viewheight)>>3;
+    }
+
+	if (player->deltaviewheight)
 	{
 	    player->deltaviewheight += FRACUNIT/4;
 	    if (!player->deltaviewheight)
@@ -151,9 +168,10 @@ void P_MovePlayer (player_t* player)
 {
     ticcmd_t*		cmd;
     int		look;
-	
+    int     move;
+
     cmd = &player->cmd;
-	
+
     player->mo->angle += (cmd->angleturn<<FRACBITS);
 
     // Do not let the player control movement
@@ -161,22 +179,25 @@ void P_MovePlayer (player_t* player)
     onground = (player->mo->z <= player->mo->floorz);
     // [crispy] give full control in no-clipping mode
     onground |= (player->mo->flags & MF_NOCLIP);
-	
+
+    move = 2048;
+    if (player->mo->flags & MF_CROUCH) {move /= 2;} // [IRamm] Check for crouching
+
     if (cmd->forwardmove && onground)
-	P_Thrust (player, player->mo->angle, cmd->forwardmove*2048);
+	P_Thrust (player, player->mo->angle, cmd->forwardmove*move);
     else
     // [crispy] in-air movement is only possible with jumping enabled
     if (cmd->forwardmove && critical->jump)
         P_Thrust (player, player->mo->angle, FRACUNIT >> 8);
-    
+
     if (cmd->sidemove && onground)
-	P_Thrust (player, player->mo->angle-ANG90, cmd->sidemove*2048);
+	P_Thrust (player, player->mo->angle-ANG90, cmd->sidemove*move);
     else
     // [crispy] in-air movement is only possible with jumping enabled
     if (cmd->sidemove && critical->jump)
             P_Thrust(player, player->mo->angle, FRACUNIT >> 8);
 
-    if ( (cmd->forwardmove || cmd->sidemove) 
+    if ( (cmd->forwardmove || cmd->sidemove)
 	 && player->mo->state == &states[S_PLAY] )
     {
 	P_SetMobjState (player->mo, S_PLAY_RUN1);
@@ -205,7 +226,7 @@ void P_MovePlayer (player_t* player)
 	                          LOOKDIRMAX * MLOOKUNIT,
 	                          player->lookdir + cmd->lookdir);
     }
-}	
+}
 
 
 
@@ -222,7 +243,7 @@ void P_DeathThink (player_t* player)
     angle_t		delta;
 
     P_MovePsprites (player);
-	
+
     // fall to the ground
     if (player->viewheight > 6*FRACUNIT)
 	player->viewheight -= FRACUNIT;
@@ -233,16 +254,16 @@ void P_DeathThink (player_t* player)
     player->deltaviewheight = 0;
     onground = (player->mo->z <= player->mo->floorz);
     P_CalcHeight (player);
-	
+
     if (player->attacker && player->attacker != player->mo)
     {
 	angle = R_PointToAngle2 (player->mo->x,
 				 player->mo->y,
 				 player->attacker->x,
 				 player->attacker->y);
-	
+
 	delta = angle - player->mo->angle;
-	
+
 	if (delta < ANG5 || delta > (unsigned)-ANG5)
 	{
 	    // Looking at killer,
@@ -259,7 +280,7 @@ void P_DeathThink (player_t* player)
     }
     else if (player->damagecount)
 	player->damagecount--;
-	
+
 
     if (player->cmd.buttons & BT_USE)
 	player->playerstate = PST_REBORN;
@@ -274,7 +295,7 @@ void P_PlayerThink (player_t* player)
 {
     ticcmd_t*		cmd;
     weapontype_t	newweapon;
-	
+
     // [AM] Assume we can interpolate at the beginning
     //      of the tic.
     player->mo->interp = true;
@@ -299,7 +320,7 @@ void P_PlayerThink (player_t* player)
 	player->mo->flags |= MF_NOCLIP;
     else
 	player->mo->flags &= ~MF_NOCLIP;
-    
+
     // chain saw run forward
     cmd = &player->cmd;
     if (player->mo->flags & MF_JUSTATTACKED)
@@ -309,8 +330,8 @@ void P_PlayerThink (player_t* player)
 	cmd->sidemove = 0;
 	player->mo->flags &= ~MF_JUSTATTACKED;
     }
-			
-	
+
+
     // [crispy] center view
     // e.g. after teleporting, dying, jumping and on demand
     if (player->centering)
@@ -357,7 +378,10 @@ void P_PlayerThink (player_t* player)
     {
         player->jumpTics--;
     }
-    
+
+    // [IRamm] Just like jumping; delay crouching
+    if (player->crouchTics) {player->crouchTics--;}
+
     // Move around.
     // Reactiontime is used to prevent movement
     //  for a bit after a teleport.
@@ -365,41 +389,76 @@ void P_PlayerThink (player_t* player)
 	player->mo->reactiontime--;
     else
 	P_MovePlayer (player);
-    
+
     P_CalcHeight (player);
 
     if (player->mo->subsector->sector->special)
 	P_PlayerInSpecialSector (player);
-    
+
     // [crispy] jumping: apply vertical momentum
     if (cmd->arti)
     {
-        if ((cmd->arti & AFLAG_JUMP) && onground && !player->jumpTics)
+        if ((cmd->arti & AFLAG_JUMP) && onground
+            && !(player->jumpTics) && !(player->crouchTics))
         {
-            // [crispy] Hexen sets 9; Strife adds 8
-            player->mo->momz = (7 + crispy->jump) * FRACUNIT;
-            player->jumpTics = 18;
-            // [crispy] squat down weapon sprite a bit
-            if (crispy->weaponsquat)
+            if (player->mo->flags & MF_CROUCH) {
+                // [IRamm] Check if ceiling's high enough to stand up.
+                if ((player->mo->ceilingz - player->mo->floorz)
+                    >= (player->mo->height)) { // [IRamm] Stand up
+                    player->mo->flags &= ~MF_CROUCH;
+                    player->mo->height = player->mo->height * 2;
+                    player->crouchTics = 18;
+                }
+            }
+            else
             {
-                player->psp_dy_max = -player->mo->momz>>2;
+                // [crispy] Hexen sets 9; Strife adds 8
+                player->mo->momz = (7 + crispy->jump) * FRACUNIT;
+                player->jumpTics = 18;
+                // [crispy] squat down weapon sprite a bit
+                if (crispy->weaponsquat)
+                {
+                    player->psp_dy_max = -player->mo->momz>>2;
+                }
             }
         }
+        // [IRamm] Crouching
+        else if (cmd->arti & AFLAG_CROUCH && !player->crouchTics) {
+            if (player->mo->flags & MF_CROUCH) {
+                // [IRamm] Check if ceiling's high enough to stand up.
+                if ((player->mo->ceilingz - player->mo->floorz)
+                    >= (player->mo->height)) { // [IRamm] Stand up
+                    player->mo->flags &= ~MF_CROUCH;
+                    player->mo->height = player->mo->height * 2;
+                    player->crouchTics = 18;
+                }
+            }
+            else { // [IRamm] Crouch
+                player->mo->flags |= MF_CROUCH;
+                player->mo->height = player->mo->height / 2;
+                player->crouchTics = 18;
+            }
+        }
+    }
+
+    // [IRamm] Use crouching player sprites when crouching
+    if (player->mo->flags & MF_CROUCH) {
+    mo->state->sprite = SPR_PLYC;
     }
 
     // Check for weapon change.
 
     // A special event has no other buttons.
     if (cmd->buttons & BT_SPECIAL)
-	cmd->buttons = 0;			
-		
+	cmd->buttons = 0;
+
     if (cmd->buttons & BT_CHANGE)
     {
 	// The actual changing of the weapon is done
 	//  when the weapon psprite can do it
 	//  (read: not in the middle of an attack).
 	newweapon = (cmd->buttons&BT_WEAPONMASK)>>BT_WEAPONSHIFT;
-	
+
 	if (newweapon == wp_fist
 	    && player->weaponowned[wp_chainsaw]
 	    && !(player->readyweapon == wp_chainsaw
@@ -407,15 +466,15 @@ void P_PlayerThink (player_t* player)
 	{
 	    newweapon = wp_chainsaw;
 	}
-	
+
 	if ( (crispy->havessg)
-	    && newweapon == wp_shotgun 
+	    && newweapon == wp_shotgun
 	    && player->weaponowned[wp_supershotgun]
 	    && player->readyweapon != wp_supershotgun)
 	{
 	    newweapon = wp_supershotgun;
 	}
-	
+
 
 	if (player->weaponowned[newweapon]
 	    && newweapon != player->readyweapon)
@@ -430,7 +489,7 @@ void P_PlayerThink (player_t* player)
 	    }
 	}
     }
-    
+
     // check for use
     if (cmd->buttons & BT_USE)
     {
@@ -448,36 +507,36 @@ void P_PlayerThink (player_t* player)
     }
     else
 	player->usedown = false;
-    
+
     // cycle psprites
     P_MovePsprites (player);
-    
+
     // Counters, time dependend power ups.
 
     // Strength counts up to diminish fade.
     if (player->powers[pw_strength])
-	player->powers[pw_strength]++;	
-		
+	player->powers[pw_strength]++;
+
     if (player->powers[pw_invulnerability])
 	player->powers[pw_invulnerability]--;
 
     if (player->powers[pw_invisibility])
 	if (! --player->powers[pw_invisibility] )
 	    player->mo->flags &= ~MF_SHADOW;
-			
+
     if (player->powers[pw_infrared])
 	player->powers[pw_infrared]--;
-		
+
     if (player->powers[pw_ironfeet])
 	player->powers[pw_ironfeet]--;
-		
+
     if (player->damagecount)
 	player->damagecount--;
-		
+
     if (player->bonuscount)
 	player->bonuscount--;
 
-    
+
     // [crispy] A11Y
     if (!a11y_invul_colormap)
     {
@@ -497,7 +556,7 @@ void P_PlayerThink (player_t* player)
 	    // [crispy] Visor effect when Invulnerability is fading out
 	    player->fixedcolormap = player->powers[pw_infrared] ? 1 : 0;
     }
-    else if (player->powers[pw_infrared])	
+    else if (player->powers[pw_infrared])
     {
 	if (player->powers[pw_infrared] > 4*32
 	    || (player->powers[pw_infrared]&8) )
