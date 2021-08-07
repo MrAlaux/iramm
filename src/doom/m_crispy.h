11d853e03654e8db85888e2f6c859bad704d0efb
Alaux <73968015+MrAlaux@users.noreply.github.com>
2021-08-07 08:39:35 -0300

    Add VIEWHEIGHT setting
    Added M_ function for VIEWHEIGHT adjustment setting

--- src/doom/m_crispy.h
+++ src/doom/m_crispy.h
@@ -85,4 +85,6 @@ extern void M_CrispyToggleVsync(int choice);
 extern void M_CrispyToggleWeaponSquat(int choice);
 extern void M_CrispyToggleWidescreen(int choice);
 
+// [IRamm]
+extern void M_IRammToggleViewheight(int choice);
 #endif
