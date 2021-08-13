9aa84939ae3b38346870f600993f72d28ba77094
Alaux <73968015+MrAlaux@users.noreply.github.com>
2021-08-13 03:49:09 -0300

    Rename P_CheckSSGGibbing to P_NuggetCheckDist
    Also added new parameters to it to make it help with the Chainsaw knockback bug "fix"

--- src/doom/p_inter.h
+++ src/doom/p_inter.h
@@ -24,7 +24,7 @@
 
 
 boolean	P_GivePower(player_t*, int);
-boolean P_CheckSSGGibbing (mobj_t*, mobj_t*); // [Nugget]
+boolean P_NuggetCheckDist(mobj_t*, mobj_t*, fixed_t range, boolean addradius); // [Nugget]
 
 
 
